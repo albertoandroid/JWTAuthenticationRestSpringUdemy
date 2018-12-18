b395d26eadd44706bbefb3997cb667a80e00e869
Alberto <info@androiddesdecero.com>
2018-12-18 22:44:46 +0100

    15

---
+++ src/main/java/com/example/curso/config/JwtSecurityConfig.java
@@ -0,0 +1,59 @@
+package com.example.curso.config;
+
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+import com.example.curso.security.JwtAuthenticationEntryPoint;
+import com.example.curso.security.JwtAuthenticationProvider;
+import com.example.curso.security.JwtAuthenticationTokenFilter;
+
+@EnableGlobalMethodSecurity(prePostEnable = true)
+@EnableWebSecurity
+@Component
+public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private JwtAuthenticationProvider authenticationProvider;
+	
+	@Autowired
+	private JwtAuthenticationEntryPoint entryPoint;
+	
+	@Bean
+	public AuthenticationManager authenticationManager() {
+		return new ProviderManager(Collections.singletonList(authenticationProvider));
+	}
+	
+	@Bean
+	public JwtAuthenticationTokenFilter authenticationTokenFilter() {
+		JwtAuthenticationTokenFilter fitler = new JwtAuthenticationTokenFilter();
+		filter.setAuthenticationManger(authenticationManager());
+		filter.setAuthenticationSuccessHandler(new JwtSuccessHandler());
+		return filter;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.csrf().disable()
+		.authorizeRequests().antMatchers("**/api/**").authenticated()
+		.and()
+		.exceptionHandling().authenticationEntryPoint(entryPoint)
+		.and()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		
+		http.addFilterBefore(authenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+		http.headers().cacheControl();
+
+	}
+	
+}
