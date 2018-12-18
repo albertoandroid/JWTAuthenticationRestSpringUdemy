b91e44ffcab783a1691ee314c9ae22ea4431e8c4
Alberto <info@androiddesdecero.com>
2018-12-18 20:59:57 +0100

    06

---
+++ src/main/java/com/example/curso/model/JwtUserDetails.java
@@ -0,0 +1,69 @@
+package com.example.curso.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class JwtUserDetails implements UserDetails {
+	
+	
+	private static final long serialVersionUID = 1L;
+	private String userName;
+	private String token;
+	private Long id;
+	private Collection<? extends GrantedAuthority> authorities;
+	
+	
+
+	public JwtUserDetails(String userName, String token, Long id, Collection<? extends GrantedAuthority> authorities) {
+		super();
+		this.userName = userName;
+		this.token = token;
+		this.id = id;
+		this.authorities = authorities;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
