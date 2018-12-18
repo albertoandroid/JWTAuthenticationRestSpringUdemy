4dac147111e53ea2a55b85783761768b32cf1ca7
Alberto <info@androiddesdecero.com>
2018-12-18 20:29:04 +0100

    01

---
+++ src/main/java/com/example/curso/RestjwtApplication.java
@@ -0,0 +1,14 @@
+package com.example.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestjwtApplication.class, args);
+	}
+
+}
+
