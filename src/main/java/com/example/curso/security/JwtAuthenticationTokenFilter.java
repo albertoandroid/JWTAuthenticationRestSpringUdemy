6020091563e26895a49c68ea2292b2f0a5f8b8cd
Alberto <info@androiddesdecero.com>
2018-12-18 21:23:55 +0100

    08

---
+++ src/main/java/com/example/curso/security/JwtAuthenticationTokenFilter.java
@@ -0,0 +1,49 @@
+package com.example.curso.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import com.example.curso.constants.Constants;
+import com.example.curso.model.JwtAuthenticationToken;
+
+public class JwtAuthenticationTokenFilter extends AbstractAuthenticationProcessingFilter {
+
+	protected JwtAuthenticationTokenFilter() {
+		super("/api/**");
+	}
+
+	@Override
+	public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response)
+			throws AuthenticationException, IOException, ServletException {
+		
+		String header = request.getHeader(Constants.AUTHORIZATION_HEADER);
+		
+		if(header == null || !header.startsWith(Constants.BEARER_TOKEN)) {
+			throw new RuntimeException("Jwt es incorrecto o no ha llegado nada");
+		}
+		String authenticationToken = header.substring(7);
+		JwtAuthenticationToken token = new JwtAuthenticationToken(authenticationToken);
+	
+
+		return getAuthenticationManager().authenticate(token);
+	}
+
+	@Override
+	protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+			Authentication authResult) throws IOException, ServletException {
+		super.successfulAuthentication(request, response, chain, authResult);
+		chain.doFilter(request, response);
+	}
+	
+	
+
+}
