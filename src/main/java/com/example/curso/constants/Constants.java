755618038bd03eb872ce6c3d2842b961fa42ca94
Alberto <info@androiddesdecero.com>
2018-12-18 21:04:45 +0100

    07 Constans

---
+++ src/main/java/com/example/curso/constants/Constants.java
@@ -0,0 +1,15 @@
+package com.example.curso.constants;
+
+public class Constants {
+	
+	public final static String YOUR_SECRET =  "your_secret";
+	
+	public final static String USER_ID = "userId";
+	
+	public final static String ROLE = "role";
+	
+	public final static String AUTHORIZATION_HEADER = "Authorization";
+	
+	public final static String BEARER_TOKEN = "Bearer ";
+
+}
